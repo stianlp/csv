f0a956ae103f53ddf490b84d773f7d63b5a817c0
stianlp <stianlp@gmail.com>
2015-09-01 01:08:26 +0200

    something

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 (function () {
     'use strict';
 
-
-
     $(function() {
-    //    // Handler for .ready() called.
-    //$( "#choir-image" ).animate({
-    //    opacity: 0.25,
-    //    left: "+=50",
-    //    height: "toggle"
-    //}, 5000, function() {
-    //    // Animation complete.
-    //});
 
-    $("#bottom").click(function() {
-        console.log("click")
-        $('html,body').animate({
-                scrollTop: $("#choir-image").prop('scrollHeight')},
-            'slow');
-    });
+        $('#click-bottom').click(function() {
+            $('html,body').animate({
+                    scrollTop: $("#choir-image").prop('scrollHeight')},
+                {
+                    duration: 1000,
+                    complete: function () {
+                        console.log('hey');
+                        //$('#header').addClass('bg');
+                    }
+                }
+            );
+        });
+
+        $(window).scroll(function() {
+            console.log(document.getElementById("page").getBoundingClientRect().top);
+            if (document.getElementById("page").getBoundingClientRect().top <= 0) {
+                $('#second-header').show('slow');
+            } else {
+                $('#second-header').hide(500);
+            }
+        });
 
     });
 }());
